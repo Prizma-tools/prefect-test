ab75b648d753e3c570e772f617b288d10fc7a1b2
Alexander Schulz <shultz1994@gmail.com>
2025-02-24 16:57:54 +0500

    first commit

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
+from prefect import flow, serve
+
+
+@flow
 def main():
     print("Hello from test-prefect!")
 
 
 if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        hello = Deployment.build_from_flow(
+            flow=main,
+            name="test-prefect",
+            tags=["test"],
+            schedule=CronSchedule(cron="0 0 * * *"),  # ✅ Указываем без `parameters`
+        )
+
+        hello.apply()  # ✅ Отправляет деплой напрямую на Prefect Server
